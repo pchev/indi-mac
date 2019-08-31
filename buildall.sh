fbf91efe8725aa74ea2288444e3527f9c5e5031f
Patrick Chevalley <pch@ap-i.net>
2019-08-31 13:42:25 +0200

    Print log in case of error

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,11 @@
 rm -rf testapp
 source build_env
 ./build_indi.sh >build_indi.log 2>&1
-if [[ $? != 0 ]]; then exit 1; fi
+if [[ $? != 0 ]]; then tail build_indi.log; exit 1; fi
 ./install_indi.sh > install_indi.log 2>&1
-if [[ $? != 0 ]]; then exit 1; fi
+if [[ $? != 0 ]]; then tail install_indi.log; exit 1; fi
 ./install_libraries.sh >install_libraries.log 2>&1
-if [[ $? != 0 ]]; then exit 1; fi
+if [[ $? != 0 ]]; then tail install_libraries.log; exit 1; fi
 ./install_tar.sh >install_tar.log 2>&1
-if [[ $? != 0 ]]; then exit 1; fi
+if [[ $? != 0 ]]; then tail install_tar.log; exit 1; fi
 
